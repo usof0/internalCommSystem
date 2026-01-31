5cb7dbfe9d39c34331b5a7d30e4153a4b4781cde
Yousef <yousef2002316@gmail.com>
2026-01-31 16:45:24 +0300

    one two three

--- server/src/rbac/rbac.module.ts
+++ server/src/rbac/rbac.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { RbacService } from "./rbac.service";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { RbacController } from './rbac.controller';
+import { RoomRbacController } from './room-rbac.controller';
 
 @Module({
   imports: [PrismaModule],
   providers: [RbacService],
   exports: [RbacService],
-  controllers: [RbacController],
+  controllers: [RbacController, RoomRbacController],
 })
 export class RbacModule {}
