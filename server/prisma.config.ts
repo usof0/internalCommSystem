78960d4592c8e415c129c12cf99c29bf24ccaf90
Yousef <yousef2002316@gmail.com>
2026-06-08 22:25:18 +0300

    fixed for deployment

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "ts-node ./prisma/seed.ts",
+    seed: "ts-node -T -P tsconfig.seed.json ./prisma/seed.ts",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
